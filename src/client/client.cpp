060f824d098253b61f79a7a2b60c47966abfd7af
Gregor Parzefall <gregor.parzefall@posteo.de>
2024-05-27 19:21:55 +0200

    First implementation of display insets API

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1455,6 +1455,7 @@ void Client::sendUpdateClientInfo(const ClientDynamicInfo& info)
 	pkt << info.real_hud_scaling;
 	pkt << (f32)info.max_fs_size.X << (f32)info.max_fs_size.Y;
 	pkt << info.touch_controls;
+	pkt << info.insets.UpperLeftCorner << info.insets.LowerRightCorner;
 
 	Send(&pkt);
 }

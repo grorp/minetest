01fa22911feb05fb6e1c3605b6da3aa6a2a8ccb8
Gregor Parzefall <gregor.parzefall@posteo.de>
2024-08-27 16:30:45 +0200

    Test 1

--- builtin/game/init.lua
+++ builtin/game/init.lua
@@ -43,3 +43,78 @@ dofile(uipath .. "init.lua")
 core.after(0, builtin_shared.cache_content_ids)
 
 profiler = nil
+
+ui.set_default_theme(ui.Style{
+	sel = "root",
+
+	rel_pos = {0.5, 0.5},
+	rel_anchor = {0.5, 0.5},
+})
+
+
+local function gui_builder(id, player, cx, param)
+	return ui.Window{
+		type = "gui",
+		root = ui.Root{
+			bg_fill = "#fff",
+			fg_image = "default_obsidian.png",
+			fg_scale = 0,
+
+			ui.Elem {
+				rel_size = {0,0},
+				size = {200, 200},
+				rel_pos = {0.5, 1},
+				rel_anchor = {0.5, 1},
+				padding = {12, 12, 12, 12},
+				-- margin = {24, 24, 24, 24},
+
+				bg_image = "default_dirt.png",
+				bg_tint = "#ffffff",
+
+				fg_image = "blank.png",
+				fg_scale = 0,
+			},
+			ui.Elem {
+
+			},
+		},
+	}
+end
+
+local function news_popup(id, player, cx, param)
+	local H = 100
+	local W = 300
+	local SPACE = 10
+	local INNER_W = W - 4*SPACE
+	local INNER_H = H - 4*SPACE
+	return ui.Window{
+		type = "message",
+		root = ui.Root{
+			size = {W, H},
+			rel_pos = {1, 1},
+			rel_anchor = {1, 1},
+			margin = {SPACE, SPACE, SPACE, SPACE},
+			padding = {SPACE, SPACE, SPACE, SPACE},
+
+			rel_size = {0, 0},
+			bg_fill = "#fff",
+
+			ui.Elem {
+				fg_image = "default_obsidian.png",
+				fg_scale = 0,
+				rel_size = {0, 0},
+				size = {INNER_H, INNER_H},
+			},
+			ui.Elem {
+				bg_image = "default_dirt.png",
+				rel_size = {0, 0},
+				margin = {INNER_H + SPACE, 0, 0, 0},
+				size = {INNER_W, INNER_H},
+			}
+		},
+	}
+end
+
+minetest.register_on_joinplayer(function(player)
+	local id = ui.open(news_popup, player:get_player_name())
+end)

bb026137e754a8f57bfa5a8f75a0f81447569b39
grorp <grorp@posteo.de>
2025-01-01 17:52:35 +0100

    Add TODO

--- builtin/common/settings/settingtypes.lua
+++ builtin/common/settings/settingtypes.lua
@@ -408,6 +408,15 @@ function settingtypes.parse_config_file(read_all, parse_mods)
 		file:close()
 	end
 
+	-- TODO: Support game/mod settings in the pause menu too
+	-- Note that this will need to work different from how it's done in the
+	-- mainmenu:
+	-- * Only if in singleplayer / on local server, not on remote servers
+	-- * Only show settings for the active game and mods
+	--   (add API function to get them, can return nil if on a remote server)
+	--   (names are probably not enough, will need paths for uniqueness)
+	-- This means just making "pkgmgr.lua" work won't get you very far.
+
 	if INIT == "mainmenu" and parse_mods then
 		-- Parse games
 		local games_category_initialized = false

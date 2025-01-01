20f6d70f7ba798e303f17b3e3f39fc2e8b9dc159
grorp <grorp@posteo.de>
2025-01-01 18:08:42 +0100

    Refactor duplicated CSM registration code

--- builtin/pause_menu/init.lua
+++ builtin/pause_menu/init.lua
@@ -1,9 +1,12 @@
 local scriptpath = core.get_builtin_path()
-local clientpath = scriptpath.."client"..DIR_DELIM
+local pausepath = scriptpath.."pause_menu"..DIR_DELIM
 local commonpath = scriptpath.."common"..DIR_DELIM
 
 -- we're in-game, so no absolute paths are needed
 defaulttexturedir = ""
 
-dofile(clientpath .. "register.lua")
+local builtin_shared = {}
+
+assert(loadfile(commonpath .. "register.lua"))(builtin_shared)
+assert(loadfile(pausepath .. "register.lua"))(builtin_shared)
 dofile(commonpath .. "settings" .. DIR_DELIM .. "init.lua")
